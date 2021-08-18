01c5d0e6def373271adea0a813fdddea60c1c383
taeung123 <youtanlai@gmail.com>
2021-08-18 09:35:04 +0700

    add validate chart analytic

--- src/app/Http/Controllers/Api/Admin/AnalyticsQueryController.php
+++ src/app/Http/Controllers/Api/Admin/AnalyticsQueryController.php
@@ -42,6 +42,25 @@ class AnalyticsQueryController extends ApiController
     public function chartAnalyticsQuery(Request $request, $slug)
     {
         $this->validator->isValid($request, 'RULE_ADMIN_REQUEST');
+        if (strtotime($request->from_date) > strtotime($request->to_date)) {
+            throw new Exception("from_date phải nhỏ hơn to_date");
+        }
+        $diff = date_diff(date_create($request->from_date), date_create($request->to_date));
+        switch ($request->date_type) {
+            case 'day':
+                if ($diff->days > 7)
+                    throw new Exception("Khoảng ngày không được quá 7 ngày");
+                break;
+            case 'week':
+                if (($diff->days / 7) > 8)
+                    throw new Exception("Khoảng tuần không được quá 8 tuần");
+                break;
+            case 'month':
+                if (($diff->m > 6) || ($diff->m == 6 && $diff->d > 0)) {
+                    throw new Exception("Khoảng tháng không được quá 6 tháng");
+                }
+                break;
+        }
         $data = $this->repository->findBySlug('chart', $request->date_type, $slug);
         $trans = [":from" => "'" . $request->from_date . "'", ':to' => "'" . $request->to_date . "'"];
         $tring_query = strtr($data->query, $trans);
